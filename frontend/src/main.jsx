0e2880d7e1400769f3f7ef4ba9ecc47e09289f0d
Mikie <kcmadhu081@gmail.com>
2024-03-29 22:13:39 +0545

    hi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,8 +9,8 @@ import axios from "axios";
 import UserContextProvider from "./Context/UserContext.jsx";
 import SearchContextProvider from "./Context/SearchContext.jsx";
 axios.defaults.baseURL =
-  "http://localhost:5001/api/v1" ||
-  "https://sync-in-circle-backend.vercel.app/api/v1";
+  "https://sync-in-circle-backend.vercel.app/api/v1" ||
+  "http://localhost:5001/api/v1";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
